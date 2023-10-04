f4bb3b7e6827c62f0a9ecf45620274eb6141ca97
technical_aks <105633032+technicalaks@users.noreply.github.com>
2023-10-04 14:27:57 +0530

    Fixed

--- Plugins/forward.py
+++ Plugins/forward.py
@@ -6,14 +6,13 @@ from pyrogram import filters
 from bot import channelforward
 from config import Config 
 
-
 @channelforward.on_message(filters.channel)
 async def forward(client, message):
     # Forwarding the messages to the channel
    try:
       for id in Config.CHANNEL:
          from_channel, to_channel = id.split(":")
-         if m.chat.id == int(from_channel):
+         if message.chat.id == int(from_channel):
             func = message.copy if AS_COPY else message.forward
             await func(int(to_channel), as_copy=True)
             logger.info("Forwarded a message from", from_channel, "to", to_channel)
